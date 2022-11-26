23b8dd2d98aa6ca7ca87e4d03f5633bfca211eb5
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2022-11-26 17:24:37 +0200

    new commit

--- task6/script.js
+++ task6/script.js
@@ -1,24 +1,4 @@
-var myevent = new Event("wait");
-var flag = true;
-document.body.onkeydown = function() {
-    flag = false;
-}
-
-document.body.addEventListener("wait", function () {
-    alert("please enter data");
-});
-
-setTimeout(function () {
-    console.log("timeout");
-    if(flag) {
-        document.body.dispatchEvent(myevent);
-    }
-},2000);
-
-var form = document.getElementsByTagName("form")[0];
-form.onsubmit = function () {
-    var flag = confirm("do you want to proceed");
-    if(!flag) {
-        event.preventDefault();
-    }
+window.oncontextmenu = function (event) {
+    alert("right click isn't allowed");
+    event.preventDefault();
 }
